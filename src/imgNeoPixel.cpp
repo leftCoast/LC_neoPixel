896ab3ff4b931ad7e2d9f528afb64a71df0c84af
Jim Lee <gitHub@leftcoast.biz>
2021-04-24 17:41:08 -0700

    Image file to neoPixel string works!
    Finally got imgNeoPixel class up and running. Takes a baseImage object address and a Adafruit neoPixel object address. Can be run in low memory mode or fully buffered for vastly better performance.

---
+++ src/imgNeoPixel.cpp
@@ -0,0 +1,85 @@
+#include <imgNeoPixel.h>
+#include <resizeBuff.h>
+
+
+imgNeoPixel::imgNeoPixel(Adafruit_NeoPixel* theLEDs,baseImage* theBMPImage) {
+	
+	mPixels			= theLEDs;
+	mImage			= theBMPImage;
+	mRGBArray		= NULL;
+	mNumPixels		= 0;
+	mImageFile		= NULL;
+}
+				
+				
+imgNeoPixel::~imgNeoPixel(void) {
+
+	resizeBuff(0,&mRGBArray);
+	if (mFile) {
+		mFile.close();
+	}
+}
+	
+	
+bool  imgNeoPixel::setupOffscreen(int numPixels) {
+
+	int		maxWidth;
+	
+	mImageFile = NULL;														// Just in case.
+	maxWidth = min(mPixels->numPixels(),mImage->getWidth());
+	if (numPixels==0) {
+		mNumPixels = maxWidth;
+	} else {
+		mNumPixels = min(maxWidth,mNumPixels);
+	}
+	if (resizeBuff(sizeof(RGBpack)*mNumPixels,&mRGBArray)) {
+		mFile = SD.open(mImage->getDocFilePath());
+		if (mFile) {
+			mImageFile = &mFile;
+			return true;
+		} else {
+			resizeBuff(0,&mRGBArray);
+			mNumPixels = 0;
+		}
+	}
+	return false;
+}
+	
+	
+void imgNeoPixel::clearOffscreen(void) {
+
+	resizeBuff(0,&mRGBArray);
+	mNumPixels = 0;
+	if (mFile) {
+		mFile.close();
+	}
+	mImageFile = NULL;
+}
+
+
+void imgNeoPixel::setLine(int row,int numPixels) {
+	
+	int		maxWidth;	
+	colorObj	aPixel;
+	
+	maxWidth = min(mPixels->numPixels(),mImage->getWidth());
+	if (numPixels==0) {
+		numPixels = maxWidth;
+	} else {
+		mNumPixels = min(maxWidth,mNumPixels);
+	}
+	if (mImageFile && mRGBArray) { 
+		mImage->getRow(row,mRGBArray,mNumPixels,mImageFile);
+		for (int i=0;i<mNumPixels;i++) {
+			mPixels->setPixelColor(i,mRGBArray[i].r,mRGBArray[i].g,mRGBArray[i].b);
+		}
+	} else {
+		for (int i=0;i<maxWidth;i++) {
+			aPixel = mImage->getPixel(i,row);
+			mPixels->setPixelColor(i,aPixel.getRed(),aPixel.getGreen(),aPixel.getBlue());
+		}
+	}
+}			
+
+	
+
---
+++ src/imgNeoPixel.h
@@ -0,0 +1,24 @@
+
+#include <baseImage.h>
+#include <Adafruit_NeoPixel.h>
+
+
+class imgNeoPixel {
+							
+	public:
+				imgNeoPixel(Adafruit_NeoPixel* theLEDs,baseImage* theBMPImage);
+	virtual	~imgNeoPixel(void);
+	
+	virtual	void	setLine(int row,int numPixels=0);
+				bool	setupOffscreen(int numPixels=0);
+				void	clearOffscreen(void);
+	
+	protected:
+				Adafruit_NeoPixel*	mPixels;
+				baseImage*				mImage;
+				RGBpack* 				mRGBArray;
+				int						mNumPixels;
+				File						mFile;
+				File*						mImageFile;
+};
+
